ee1723ddc26ff82afd34327d27b236700039c19e
luisramos <luis.mario@kunaico.com>
2023-09-02 12:07:02 -0600

    Follow Factory Pattern

--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -2,11 +2,18 @@
 
 namespace SchoolHRAdministration
 {
+    public enum EmployeeType
+    {
+        Teacher,
+        HeadOfDepartment,
+        DeputyHeadMaster,
+        HeadMaster
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            decimal totalSalaries = 0;
+            //decimal totalSalaries = 0;
             var employees = new List<IEmployee>();
 
             SeedData(employees);
@@ -18,7 +25,7 @@ namespace SchoolHRAdministration
 
             // Console.WriteLine($"Total Annual Salaries (including bonus): {totalSalaries}");
 
-            System.Console.WriteLine($"Total Annual Salaries (including bonus): {employees.Sum(e => e.Salary)}");
+            Console.WriteLine($"Total Annual Salaries (including bonus): {employees.Sum(e => e.Salary)}");
 
             Console.ReadKey();
 
@@ -26,55 +33,21 @@ namespace SchoolHRAdministration
 
         static void SeedData(List<IEmployee> employees)
         {
-            IEmployee teacher1 = new Teacher
-            {
-                Id = 1,
-                FirstName = "Bob",
-                LastName = "Fisher",
-                Salary = 40000
-            };
-
+            IEmployee teacher1 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Bob", "Fisher", 40000);
             employees.Add(teacher1);
 
-            IEmployee teacher2 = new Teacher
-            {
-                Id = 1,
-                FirstName = "Jenny",
-                LastName = "Thomas",
-                Salary = 40000
-            };
-
+            IEmployee teacher2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 2, "Jenny", "Thomas", 40000);
             employees.Add(teacher2);
 
-            IEmployee headOfDepartment = new HeadOfDepartment
-            {
-                Id = 1,
-                FirstName = "Brenda",
-                LastName = "Mullins",
-                Salary = 50000
-            };
-
+            IEmployee headOfDepartment = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 3, "Brenda", "Mullins", 50000);
             employees.Add(headOfDepartment);
 
-            IEmployee deputyHeadMaster = new DeputyHeadMaster
-            {
-                Id = 1,
-                FirstName = "Devlin",
-                LastName = "Brown",
-                Salary = 60000
-            };
-
+            IEmployee deputyHeadMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 4, "Devlin", "Brown", 60000);
             employees.Add(deputyHeadMaster);
 
-            IEmployee headMaster = new HeadMaster
-            {
-                Id = 1,
-                FirstName = "Damien",
-                LastName = "Jones",
-                Salary = 80000
-            };
-
+            IEmployee headMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 5, "Demien", "Jones", 80000);
             employees.Add(headMaster);
+
         }
     }
 
@@ -95,5 +68,34 @@ namespace SchoolHRAdministration
     class HeadMaster : EmployeeBase
     {
         public override decimal Salary { get => base.Salary + (base.Salary * 0.05m); }
+    }
+
+    public static class EmployeeFactory
+    {
+        public static IEmployee GetEmployeeInstance(EmployeeType employeeType, int id, string firstName, string lastName, decimal salary)
+        {
+            IEmployee employee = null;
+
+            switch (employeeType)
+            {
+                case EmployeeType.Teacher:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.HeadOfDepartment:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.DeputyHeadMaster:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.HeadMaster:
+                    employee = new HeadMaster { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+
+                default:
+                    break;
+            }
+
+            return employee;
+        }
     }
 }
