315fc51b4c754605d5cd5a4d10aeb105fe7febaa
luisramos <luis.mario@kunaico.com>
2023-09-01 19:38:15 -0600

    Add hardcoded employees

--- HRAdministrationAPI/IEmployee.cs
+++ HRAdministrationAPI/IEmployee.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HRAdministrationAPI
 {
-    internal interface IEmployee
+    public interface IEmployee
     {
         int Id { get; set; }
         string? FirstName { get; set; }
--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HRAdministrationAPI;
+
+namespace SchoolHRAdministration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void SeedData(List<IEmployee> employees)
+        {
+            IEmployee teacher1 = new Teacher
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Fisher",
+                Salary = 40000
+            };
+
+            employees.Add(teacher1);
+
+            IEmployee teacher2 = new Teacher
+            {
+                Id = 1,
+                FirstName = "Jenny",
+                LastName = "Thomas",
+                Salary = 40000
+            };
+
+            employees.Add(teacher2);
+
+            IEmployee headOfDepartment = new HeadOfDepartment
+            {
+                Id = 1,
+                FirstName = "Brenda",
+                LastName = "Mullins",
+                Salary = 50000
+            };
+
+            employees.Add(headOfDepartment);
+
+            IEmployee deputyHeadMaster = new DeputyHeadMaster
+            {
+                Id = 1,
+                FirstName = "Devlin",
+                LastName = "Brown",
+                Salary = 60000
+            };
+
+            employees.Add(deputyHeadMaster);
+
+            IEmployee headMaster = new HeadMaster
+            {
+                Id = 1,
+                FirstName = "Damien",
+                LastName = "Jones",
+                Salary = 80000
+            };
+
+            employees.Add(headMaster);
+        }
+    }
+
+    class Teacher : EmployeeBase
+    {
+
+    }
+
+    class HeadOfDepartment : EmployeeBase
+    {
+
+    }
+    class DeputyHeadMaster : EmployeeBase
+    {
+
+    }
+
+    class HeadMaster : EmployeeBase
+    {
+
+    }
+}
