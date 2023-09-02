4e8d87819b69f49225c98ff87b2079fb72bf7710
luisramos <luis.mario@kunaico.com>
2023-09-01 19:18:29 -0600

    Employee Base class

--- HRAdministrationAPI/EmployeeBase.cs
+++ HRAdministrationAPI/EmployeeBase.cs
@@ -9,8 +9,8 @@ namespace HRAdministrationAPI
 
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public virtual decimal Salary { get; set; }
     }
 }
